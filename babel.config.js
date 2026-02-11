07487599e0a347c3b74d026adb96ef7f68c30d7f
Gidiexpress <maxemmily@gmail.com>
2026-02-11 13:21:23 +0100

    feat: add Babel plugin to inline `EXPO_PUBLIC_PROJECT_ID` and `EXPO_PUBLIC_AUTH_BROKER_URL` environment variables.

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['transform-inline-environment-variables', {
+        include: ['EXPO_PUBLIC_PROJECT_ID', 'EXPO_PUBLIC_AUTH_BROKER_URL']
+      }]
+    ],
   };
 };
